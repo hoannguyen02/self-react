701f52eec392c500d5fae94f3d245be39f85c0b1
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2020-03-15 20:45:24 +0700

    Add core bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.scss';
 import Header from './shared/components/header/Header';
 import Footer from './shared/components/footer/Footer';
+import './shared/scss/bootstrap-grid.scss';
 
 const App = () => {
   return (
     <>
       <Header />
-      <main>
-        <h2>Self React</h2>
+      <main className="container">
+        <h2 className="text-center">Self React</h2>
       </main>
       <Footer />
     </>
